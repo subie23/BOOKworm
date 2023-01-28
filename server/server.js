48266b1c490d4cef788f342e9d27bfce21e5d2f7
Suzanne Banasihan <sjb04@ymail.com>
2023-01-27 22:20:11 -0800

    updating more files

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,7 +4,13 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-    authMiddleware: function ({ req }) {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+    
+  authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
     console.log("TOKEN", token);
 
@@ -18,18 +24,12 @@ module.exports = {
 
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      console.log("Data", data);
-
+      console.log("DATA", data);
+      req.user = data;
     } catch {
       console.log('Invalid token');
     }
 
     return req;
-  },
-  
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
